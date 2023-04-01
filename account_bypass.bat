786560483cbb55304b18ed3053b8578ba29c375c
Antun Nitraj <antnitraj@gmail.com>
2023-04-01 14:40:46 +0200

    Update account_bypass.bat

--- account_bypass.bat
+++ account_bypass.bat
@@ -1,1 +1,1 @@
-echo {"accounts": [{"entitlement": {"canPlayMinecraft": true,"ownsMinecraft": true},"type": "Offline"}],"formatVersion": 3} > %appdata%/PrismLauncher/accounts.json
+echo '{"accounts": [{"entitlement": {"canPlayMinecraft": true,"ownsMinecraft": true},"type": "Offline"}],"formatVersion": 3}' > %appdata%/PrismLauncher/accounts.json
